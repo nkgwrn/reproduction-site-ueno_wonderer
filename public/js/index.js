95bd5f13427afca9fd0d077c5edc602b83c36db8
Rino Nakagawara <nakagawara@reclick.co.jp>
2020-06-02 15:23:12 +0900

    add: animation

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,31 @@
-var mySwiper = new Swiper(".swiper-container", {
-  loop: true,
-  speed: 1000,
-  // autoplay: {
-  //   delay: 6000,
-  //   disableOnInteraction: false,
-  // },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+$(window).on("load", function () {
+  $(".p-cover").fadeOut();
+
+  setTimeout(function () {
+    $(".p-mv-right__title, .p-mv-right__copy").fadeIn().addClass("is-open");
+    setTimeout(function () {
+      $(".p-slide__image").addClass("is-open");
+      setTimeout(function () {
+        var mySwiper = new Swiper(".swiper-container", {
+          loop: true,
+          speed: 1000,
+          autoplay: {
+            delay: 6000,
+            disableOnInteraction: false,
+          },
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          },
+        });
+      }, 0);
+    }, 500);
+  }, 500);
 });
 
 $(function () {
-  console.log("こんにちは！");
   $(".cp_arrow").on("click", function () {
     $(this).toggleClass("is-active");
     $(".l-gnav").toggleClass("is-open");
-    console.log("こんにちは！");
   });
 });
