0024e26e7d5c016a0864b5e7f87b5c6f46f43e2f
Rino Nakagawara <nakagawara@reclick.co.jp>
2020-06-02 13:08:13 +0900

    add: sp版調整、スライダーの調整等

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,21 @@
 var mySwiper = new Swiper(".swiper-container", {
   loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+  speed: 1000,
+  // autoplay: {
+  //   delay: 6000,
+  //   disableOnInteraction: false,
+  // },
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
 });
 
 $(function () {
   console.log("こんにちは！");
   $(".cp_arrow").on("click", function () {
+    $(this).toggleClass("is-active");
     $(".l-gnav").toggleClass("is-open");
-    // if ($(".l-gnav").hasClass("is-open")) {
-    //   $(".l-header__lang").css("display", "none");
-    // } else {
-    //   $(".l-header__lang").css("display", "flex");
-    // }
     console.log("こんにちは！");
   });
 });
